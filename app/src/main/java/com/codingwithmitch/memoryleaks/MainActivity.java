8da6e34b3e1ea9a751e960af9b2492083cc913a4
Mitch Tabian <mitch@tabian.ca>
2019-01-03 11:10:24 -0800

    master

--- app/src/main/java/com/codingwithmitch/memoryleaks/MainActivity.java
+++ app/src/main/java/com/codingwithmitch/memoryleaks/MainActivity.java
@@ -2,6 +2,8 @@ package com.codingwithmitch.memoryleaks;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,53 +14,63 @@ import java.lang.ref.WeakReference;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextView;
+    private MyAsyncTask myAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = findViewById(R.id.textview);
 
 
         findViewById(R.id.go).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MyAsyncTask myAsyncTask = new MyAsyncTask(mTextView);
+
+                if(myAsyncTask != null){
+                    finish();
+                }
+
+                myAsyncTask = new MyAsyncTask(MainActivity.this);
                 myAsyncTask.execute();
 
             }
         });
-
-
     }
 
     private class MyAsyncTask extends AsyncTask<Void, Void, Void>{
 
-        private TextView mTextView;
 
-        public MyAsyncTask(TextView textView) {
-            this.mTextView = textView;
+//        private Context mContext;
+//        private WeakReference<Context> mContextRef;
+
+
+
+        public MyAsyncTask(Context context) {
+            context = null;
+//            this.mContext = context; // METHOD 1: CANCEL ASYNCTASK in onDestroy
+//            mContextRef = new WeakReference<>(context); // METHOD 2: Use WeakReference
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
 
+//            Bitmap icon = BitmapFactory.decodeResource(mContextRef.get().getResources(),R.drawable.ic_launcher_background);
+//            Bitmap icon = BitmapFactory.decodeResource(mContext.getResources(),R.drawable.ic_launcher_background);
+
             try {
-                Thread.sleep(3000);
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             return null;
         }
+    }
 
 
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            mTextView.setText("some text");
-            onDestroy();
-        }
+    @Override
+    protected void onDestroy() {
+//        myAsyncTask.cancel(true);
+        super.onDestroy();
     }
 }
 
--- app/src/main/java/com/codingwithmitch/memoryleaks/MyApplication.java
+++ app/src/main/java/com/codingwithmitch/memoryleaks/MyApplication.java
@@ -10,8 +10,12 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
             return;
         }
         LeakCanary.install(this);
+        // Normal app init code...
     }
 }
+
